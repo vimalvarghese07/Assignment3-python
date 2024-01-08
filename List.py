62da6ec30d2790bc58c76a0e0901f4c828553ffd
Vimal Varghese <118934794+vimalvarghese07@users.noreply.github.com>
2024-01-08 18:31:57 +0530

    Update List.py

--- List.py
+++ List.py
@@ -17,12 +17,12 @@ class Person:
         print("Age:",self.age)
         print("Address:",self.address)
 
-person1 = Person(name="John Doe", age=25, address="123 Main St")
+person1 = Person(name="Vimal Varghese", age=22, address="248-b vytilla")
 
 print("Initial Information:")
 person1.display_information()
 
-person1.update_information(age=26, address="456 Oak St")
+person1.update_information(age=23, address="123-C Cherthala")
 
 print("\nUpdated Information:")
 person1.display_information()
